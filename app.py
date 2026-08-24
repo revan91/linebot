95e44e6fc83699efed42419ae3aa376fae634ad6
revan91 <abcd9118172@gmail.com>
2026-08-24 23:33:49 +0800

    1

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
-    Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
 )
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 
 app = Flask(__name__)
 
 # 讀取環境變數
-CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
-CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
+CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
 
 # 印出檢查訊息至控制台 (Render Logs)
 print("=== Starting App Initialization ===")
@@ -25,18 +29,39 @@ handler = WebhookHandler(CHANNEL_SECRET or "dummy_secret")
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN or "dummy_token")
 
 LOCATIONS = [
-    "台北 101", "陽明山國家公園", "淡水老街", "九份老街",
-    "宜蘭礁溪溫泉", "台中逢甲夜市", "日月潭", "清境農場",
-    "台南奇美博物館", "高雄駁二藝術特區", "墾丁國家公園", "花蓮太魯閣"
+    "基隆市",
+    "台北市",
+    "新北市",
+    "桃園市",
+    "新竹市",
+    "苗栗縣",
+    "台中市",
+    "彰化縣",
+    "南投縣",
+    "雲林縣",
+    "嘉義市",
+    "嘉義縣",
+    "台南市",
+    "高雄市",
+    "屏東縣",
+    "宜蘭縣",
+    "花蓮縣",
+    "台東縣",
+    "澎湖縣",
+    "金門縣",
+    "連江縣",
+    "新竹縣",
 ]
 
-@app.route("/", methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def health_check():
     return "LINE Bot is running!", 200
 
-@app.route("/callback", methods=['POST'])
+
+@app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers.get('X-Line-Signature', '')
+    signature = request.headers.get("X-Line-Signature", "")
     body = request.get_data(as_text=True)
 
     try:
@@ -44,7 +69,8 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return "OK"
+
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
@@ -59,12 +85,13 @@ def handle_message(event):
             line_bot_api.reply_message(
                 ReplyMessageRequest(
                     reply_token=event.reply_token,
-                    messages=[TextMessage(text=reply_text)]
+                    messages=[TextMessage(text=reply_text)],
                 )
             )
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"Server starting on port {port}...")
     # host='0.0.0.0' 是 Render 能否連線的關鍵
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
